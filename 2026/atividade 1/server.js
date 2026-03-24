75cb2118cae0fe9c6f03e690c9346265c5f836c2
Mark <161876331+MkRiberr@users.noreply.github.com>
2026-03-24 17:10:37 -0300

    Add endpoints for user and pastel data

--- 2026/atividade 1/server.js
+++ 2026/atividade 1/server.js
@@ -7,6 +7,35 @@ app.get("/", (request,response) => {
     response.send("ola mundo!");
 });
 
+app.get("/usuario", (request,response) => {
+    response.json({
+        id:1,
+        nome:"mark",
+        profissao:"editor de video"
+    
+
+    });
+});
+
+app.get("/pastel", (request,response) => {
+    response.json([
+        {
+            id:1,
+            tipo:"flango",
+        },
+        {
+            id:2,
+            tipo:"caine",
+        },
+        {
+            id:2,
+            tipo:"atum",
+        }
+
+    ]
+    );
+});
+
 app.listen(porta, (request,response)=> {
     console.log(`Servidor rodando: http://127.0.0.1:${porta}`)
 }
